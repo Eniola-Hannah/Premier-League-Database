303fab974d4c2d634360dbeec8c9f021875c97cd
Eniola Hannah <heniolahannah@gmail.com>
2025-01-19 13:28:07 +0100

    Creates table 'manchesterUnited'

--- Club Players.sql
+++ Club Players.sql
@@ -2,4 +2,15 @@ Create Database premierleague_database;
 
 Use premierleague_database;
 
-    
+Create Table ManchesterUnited(
+	Jersey_Number INT Primary Key,
+    First_Name Varchar(50) Not Null,
+    Last_Name Varchar(50) Not Null,
+    Age INT,
+    Position Varchar(30),
+    Club_color Text,
+    Nationality Text Not Null,
+    Preferred_Foot Varchar(20),
+    Date_of_Birth Date Not Null,
+    Wage Decimal(10,2)
+);
