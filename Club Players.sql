7e0eda96b7a2a2cc29dd6a418b1dd2a79b5ec2fc
Eniola Hannah <heniolahannah@gmail.com>
2025-01-19 13:32:17 +0100

    Creates table for Arsenal club

--- Club Players.sql
+++ Club Players.sql
@@ -29,3 +29,16 @@ Create Table Chelsea(
     Date_of_Birth Date Not Null,
     Wage Decimal(10,2)
 );
+
+Create Table Arsenal(
+	Jersey_Number INT Primary Key,
+    First_Name Varchar(50) Not Null,
+    Last_Name Varchar(50) Not Null,
+    Age INT,
+    Position Varchar(30),
+    Club_color Text,
+    Nationality Text Not Null,
+    Preferred_Foot Varchar(20),
+    Date_of_Birth Date Not Null,
+    Wage Decimal(10,2)
+);
